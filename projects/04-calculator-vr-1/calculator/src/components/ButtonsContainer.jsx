2c4467c4a0c0d485159d60e05e57ef86595a3598
Akshay D S <akshayakki4898@gmail.com>
2026-02-27 23:18:53 +0530

    first react upload

---
+++ projects/04-calculator-vr-1/calculator/src/components/ButtonsContainer.jsx
@@ -0,0 +1,12 @@
+import  style  from "./ButtonsContainer.module.css";
+const ButtonsContainer = ()=>{
+const buttonName =['C', '1', '2', '+', '3', '4', '-', '5', '6', '*', '7', '8', '/', '=', '9', '0', '.']
+
+  return <div className={style.buttonscontainer}>
+    {buttonName.map(buttonName =><button className={style.button}>{buttonName}</button>
+    )}
+        
+      </div>
+}
+
+export default ButtonsContainer;
---
+++ projects/04-calculator-vr-1/calculator/src/components/Display.jsx
@@ -0,0 +1,7 @@
+import style from "./Display.module.css";
+const Display = () =>{
+  return <input type="text" placeholder="0"  className={style.display}></input>
+
+}
+
+export default Display;
