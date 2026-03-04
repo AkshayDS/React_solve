268667da22524ccf2a1891802151fde2ef72499b
Akshay D S <akshayakki4898@gmail.com>
2026-03-04 15:47:22 +0530

    new

---
+++ chef_cloud_project/cheff/src/components/Main.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+export default function Main() {
+const [ingredients, setIngredients] = React.useState(["Note: Add only the Non Veg Ingridents"])
+const ingredientItems = ingredients.map(ingredient=>(
+  <li key={ingredient}>{ingredient}</li>
+
+))
+
+function handleSubmit(formData){
+  
+  const newIngrident = formData.get("ingredient")
+  setIngredients(previous=> [...previous,newIngrident])
+  
+  
+}
+
+    return (
+        <main>
+            <form action={handleSubmit}className="add-ingredient-form">
+                <input 
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+              {ingredientItems}
+            </ul>
+        </main>
+    )
+}
